728fceb1fbb4401a3c29db00705e8e0f7918cd57
kaytonfroeschl <kfroeschl@gmail.com>
2022-04-26 20:42:39 -0500

    add

---
+++ config.php
@@ -0,0 +1,17 @@
+
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'c756b631');
+define('DB_PASSWORD', 'er4Pho3i');
+define('DB_NAME', 'Users');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
---
+++ login.php
@@ -0,0 +1,10 @@
+
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Password: <?php echo $_GET["password"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
