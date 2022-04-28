5685e3af4b8196f4cc14be71835496a4f12156d1
celiababst <celiababst@gmail.com>
2022-04-27 20:06:00 -0500

    this is the correct add to cart stuff

--- config.php
+++ config.php
@@ -1,19 +1,7 @@
 <?php
-$mysqli = new mysqli("mysql.eecs.ku.edu","c756b631","er4Pho3i","c756b631");
-
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
+// Create connection
+$conn = new mysqli("mysql.eecs.ku.edu", "c756b631", "er4Pho3i", "c756b631");
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-
-//$sql = "SELECT Lastname, Age FROM Persons ORDER BY Lastname"; 
-
-/*if ($result = $mysqli -> query($sql)) {
-  while ($obj = $result -> fetch_object()) {
-    printf("%s (%s)\n", $obj->Lastname, $obj->Age);
-  }
-  $result -> free_result();
-}*/
-
-$mysqli -> close();
-?>
